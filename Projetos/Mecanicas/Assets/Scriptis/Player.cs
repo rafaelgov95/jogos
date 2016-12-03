ebcd21a5e19c72fa4eb745abc0d9dff447c8ee00
Mr.Viana <rafaelgov95@gmail.com>
2016-12-03 08:57:56 -0300

    up

---
+++ Projetos/Mecanicas/Assets/Scriptis/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+	public GameObject player;
+	public float speed = 3f;
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetAxis("Horizontal") < 0)
+		{
+			player.transform.position = new Vector2(player.transform.position.x - speed, player.transform.position.y);
+			//player.transform.position.x += 2f;
+		}
+		else if (Input.GetAxis("Horizontal") > 0)
+		{
+			player.transform.position = 
+				new Vector2(player.transform.position.x + speed,
+				player.transform.position.y);
+			Debug.Log(Input.GetAxis("Horizontal"));
+		}
+	}
+}
---
+++ Projetos/Mecanicas/Assets/Scriptis/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	private float movX = 3;
+
+	public float speed = 40.0f;
+	public float forceJump = 300f;
+
+	private bool isRigth;
+
+	private Transform t;
+
+	// Use this for initialization
+	void Start ()
+	{
+		t = GetComponent <Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		movX = Input.GetAxis ("Horizontal");
+
+		Vector2 mov = new Vector2 (movX * speed, 0);
+
+		//GetComponent<Rigidbody2D> ().AddForce (mov);
+		GetComponent<Rigidbody2D> ().velocity = mov;
+
+		if (Input.GetButtonDown ("Jump")) {
+			//por velociadade
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, forceJump);
+			//GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, forceJump));
+		}
+
+		if (isRigth && movX > 0) {
+			Flip ();
+		} else if (!isRigth && movX < 0) {
+			Flip ();
+		}
+	
+	}
+
+	void Flip ()
+	{
+		t.Rotate (new Vector3 (0, 180, 0));
+		isRigth = !isRigth;
+	}
+}
